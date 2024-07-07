573819033cb13d38d2ba551b63303741bf2a0ef9
knottykoder <hemant.kumar472@gmail.com>
2024-07-07 17:36:01 +0530

    add navigation bar

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import bg from '../../public/background/home-background.png'
 import RenderModal from "@/components/RenderModal";
 import Wizard from "@/components/modals/Wizard";
+import Navigation from "@/components/navigation";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
    <Image src={bg} alt="background" fill className="w-full h-full object-cover object-center opacity-25"/>
 
    <div className="w-full h-screen">
+    <Navigation/>
 <RenderModal>
   <Wizard/>
 </RenderModal>
